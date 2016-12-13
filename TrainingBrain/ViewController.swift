d02a9384e88bcfad8858cc70411f130afe97e90e
David Huang <yam7611@yahoo.com.tw>
2016-12-14 10:10:08 +1100

    Initial Commit

---
+++ TrainingBrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingBrain
+//
+//  Created by yam7611 on 12/14/16.
+//  Copyright © 2016 yam7611. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
